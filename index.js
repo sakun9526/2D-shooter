a14bb14a5634254fb5d6c72a2c34a4c8c5b9dd23
sakun9526 <sakunrathnayaka@yahoo.com>
2022-04-06 19:57:21 +0530

    Player created

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const canvas = document.querySelector('canvas');
 
+// initiate canvas context 
+const ctx = canvas.getContext('2d');
+
 //set canvas width and height
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
-console.log(canvas);
+// create player 
+class Player {
+    constructor(x, y, radius, color){
+        this.x = x
+        this.y = y 
+        this.radius = radius
+        this.color = color
+    }
+
+    draw(){
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        ctx.fillStyle = this.color
+        ctx.fill()
+    }
+}
+
+// center the player regardless of the window size
+const x = canvas.width/2
+const y = canvas.height/2
+
+
+const player = new Player(x, y, 30, 'blue')
+console.log(player)
+
+player.draw()
